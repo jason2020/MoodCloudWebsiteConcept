d73f2a2408d8843c5ec1864eceb688d583b6339a
jason2020 <jtay20@punahou.edu>
2021-03-26 03:37:29 -1000

    It highlights the right one, but the tab is laggy. I know why but I don't have the brain cells to fix this right now

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,26 +4,43 @@ import { useNavigation } from '@react-navigation/native';
 
 const Nav = () => {
     const navigation = useNavigation();
-    return (
-        <Footer>
-            <FooterTab>
-                <Button active onPress={() => navigation.navigate("Home")}>
-                    <Icon active name="home" />
-                </Button>
-                <Button onPress={() => navigation.navigate("Data")}>
-                    <Icon name="podium-outline" /> 
-                </Button>
-                <Button onPress={() => navigation.navigate("Community")}>
-                    <Icon name="chatbubble-ellipses-outline"/> 
-                </Button>
-                <Button onPress={() => navigation.navigate("Apps")}>
-                    <Icon name="apps" />
-                </Button>
-                <Button onPress={() => navigation.navigate("Account")}>
-                    <Icon name="person" />
-                </Button>
-            </FooterTab>
-        </Footer>
-    );
+    const [activeButton, setActive] = React.useState(0);
+console.log(activeButton);
+return (
+    <Footer>
+        <FooterTab>
+            <Button active={activeButton == 0} onPress={() => {
+                navigation.navigate("Home");
+                setActive(0);
+            }}>
+                <Icon active name="home" />
+            </Button>
+            <Button active={activeButton == 1} onPress={() => {
+                navigation.navigate("Data");
+                setActive(1);
+            }}>
+                <Icon name="podium-outline" />
+            </Button>
+            <Button active={activeButton == 2} onPress={() => {
+                navigation.navigate("Community");
+                setActive(2);
+            }}>
+                <Icon name="chatbubble-ellipses-outline" />
+            </Button>
+            <Button active={activeButton == 3} onPress={() => {
+                navigation.navigate("Apps");
+                setActive(3);
+            }}>
+                <Icon name="apps" />
+            </Button>
+            <Button active={activeButton == 4} onPress={() => {
+                navigation.navigate("Account");
+                setActive(4);
+            }}>
+                <Icon name="person" />
+            </Button>
+        </FooterTab>
+    </Footer>
+);
 }
 export default Nav;
