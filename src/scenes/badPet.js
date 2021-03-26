234dc3ba13ece276635560531c5734e42aa0e665
jason2020 <jtay20@punahou.edu>
2021-03-26 03:54:51 -1000

    small change, not important

--- src/scenes/badPet.js
+++ src/scenes/badPet.js
@@ -14,7 +14,7 @@ const BadPet = () => {
         setTimeout(() => { clearInterval(timerId); setOffset(0);}, 1000);
     }
 
-    const phrases = ["I'm hungry. Feed me", "Hey! I mean meow.", "Woof woof", "Feed me please", "I hunger for human food", "I'm bored, feed me. I want to feel something", "Hi friend!", "Woo hoo!", "Meow!", "Haven't seen u in a while!", "Tubular!", "Wowza!", "Fantastic!", "Stop feeding me! I've eaten enough", "Please stop"]
+    const phrases = ["I'm hungry. Feed me", "Hey! I mean meow.", "Woof woof", "Feed me please", "I wonder what you're feeding me with", "I'm bored, feed me. I want to feel something", "Hi friend!", "Woo hoo!", "Meow!", "Haven't seen u in a while!", "Tubular!", "Wowza!", "Fantastic!", "Stop feeding me! I've eaten enough", "Please stop"]
     return (
         <SafeAreaView>
             <Thumbnail style={{transform: [{ rotate: `${offset}deg`}], "marginTop": `${offset}%`, "alignSelf":"center", "width":300, "height":300}} square source={{ uri:'https://img.favpng.com/11/3/4/cat-pixel-art-pusheen-png-favpng-NiGKjk3kJ7cfbSJeYVNfeaF4j.jpg' }} />
