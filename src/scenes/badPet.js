755e9b4f644d2cdea2f0b212a00ae84f2d661b15
jason2020 <jtay20@punahou.edu>
2021-03-26 03:52:51 -1000

    added fun phrases

--- src/scenes/badPet.js
+++ src/scenes/badPet.js
@@ -13,12 +13,13 @@ const BadPet = () => {
         }, 100);
         setTimeout(() => { clearInterval(timerId); setOffset(0);}, 1000);
     }
+
+    const phrases = ["I'm hungry. Feed me", "Hey! I mean meow.", "Woof woof", "Feed me please", "I hunger for human food", "I'm bored, feed me. I want to feel something", "Hi friend!", "Woo hoo!", "Meow!", "Haven't seen u in a while!", "Tubular!", "Wowza!", "Fantastic!", "Stop feeding me! I've eaten enough", "Please stop"]
     return (
         <SafeAreaView>
-            <Text>Pet Demo</Text>
             <Thumbnail style={{transform: [{ rotate: `${offset}deg`}], "marginTop": `${offset}%`, "alignSelf":"center", "width":300, "height":300}} square source={{ uri:'https://img.favpng.com/11/3/4/cat-pixel-art-pusheen-png-favpng-NiGKjk3kJ7cfbSJeYVNfeaF4j.jpg' }} />
-            <Text></Text>
             <Button onPress={feedCat} style={{"alignSelf":"center", "padding": "3%"}}><Text>Feed Me!</Text></Button>
+            <Text style={{"marginTop": 10, "alignSelf": "center"}}>{phrases[Math.floor(Math.random() * (phrases.length))]}</Text>
         </SafeAreaView>
     );
 };
